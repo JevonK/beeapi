48f1824449bd26fd19a69a2ce1cb2b5f827bce8d
zhanganjie <zhanganjie@wdwd.com>
2021-12-01 18:38:38 +0800

    11

--- main.go
+++ main.go
@@ -2,10 +2,19 @@ package main
 
 import (
 	_ "beeapi/routers"
+	"fmt"
 	beego "github.com/beego/beego/v2/server/web"
 )
 
+func init()  {
+	mysqlConfig, _ := beego.AppConfig.String("httpport")
+	fmt.Print("1231231"+mysqlConfig+"\n")
+	//orm.RegisterDriver("mysql", orm.DRMySQL)
+	//orm.RegisterDataBase("default", "mysql", mysqlConfig)
+}
+
 func main() {
+
 	// 开启swagger api 文档
 	if beego.BConfig.RunMode == "dev" {
 		beego.BConfig.WebConfig.DirectoryIndex = true
